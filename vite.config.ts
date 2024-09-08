95b81d0a4c1d2251c5e3aa118dd5954b107a3f10
Jeel <baimsnik@gmail.com>
2024-09-08 23:56:36 +0530

    Added public 404

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
 		},
 	},
 	base: '/careX/', // YOUR REPO NAME HERE
+	build: {
+		rollupOptions: {
+			input: {
+				main: './index.html',
+				404: './public/404.html', // Ensure 404.html is included in the build
+			},
+		},
+	},
 });
